48fb74a143e63fc9459522b579c29a734da902e0
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-09 20:45:55 +0200

    Converter example

--- index.js
+++ index.js
@@ -1,18 +1,62 @@
-const btn = document.querySelector('button');
-console.log(btn);
+/*
 
-const btn2 = document.querySelector('.super-btn');
-console.log(btn2);
+Завдання: Створити конвертер валют
 
-const btn3 = document.querySelector('#btnbtn');
-console.log(btn3);
+Декомпозиція:
 
-const btnCollection = document.querySelectorAll('button');
-console.log(btnCollection);
++ 1. Створити HTML-сторінку з формою, яка містить два поля вводу:
+одне поле для введення суми в одній валюті
+інше поле - для вибору валюти, в яку ми будемо цю суму конвертувати
 
-const paragraphCollection = document.querySelectorAll('.paragraph:nth-child(even)');
-console.log(paragraphCollection);
++ 2. Додати в форму кнопку конвертувати
 
-for(let p of paragraphCollection) {
-  p.style.color = 'red';
+3. Написати JS обробник події відправки форми:
+- отримати значення введеної суми та обраної валюти
+- ообчислити конвертовану суму, помноживши введену користувачем суму на курс обміну вибраної користувачем валюти
+- вивести результат конвертації на сторінку для відображення результату (innerHTML)
+
+4. Підписати форму на подію "submit", в якості лісенера використовувати функцію з п. 3
+
+*/
+
+const CURRENCY = {
+  USD_TO_UAH: 37.65,
+  EUR_TO_UAH: 40.61,
+}
+
+const converterForm = document.querySelector('#currency-converter-form');
+
+converterForm.addEventListener('submit', convertHandler)
+
+function convertHandler(event) {
+  event.preventDefault();
+
+  const amount = Number(document.querySelector('#amount').value);
+  const currency = document.querySelector('#currency').value;
+
+  let convertedAmount;
+  const {USD_TO_UAH: usdCourse, EUR_TO_UAH: eurCourse} = CURRENCY;
+
+  switch (currency) {
+    case 'USD': {
+      convertedAmount = amount * usdCourse;
+      break;
+    }
+    case 'EUR': {
+      convertedAmount = amount * eurCourse;
+      break
+    }
+    default: throw new Error('Something gone wrong!');
+  }
+
+  // if(currency === 'USD') {
+  //   convertedAmount = amount * usdCourse;
+  // } else if (currency === 'EUR') {
+  //   convertedAmount = amount * eurCourse;
+  // } else {
+  //   throw new Error('Something gone wrong!');
+  // }
+
+  const relustDiv = document.querySelector('#result');
+  relustDiv.innerHTML = `${amount} ${currency} = ${convertedAmount.toFixed(2)} UAH`;
 }
