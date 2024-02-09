8f188e36ae14ba05dfc43fe7b67dc6d8d35f3154
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-09 18:41:39 +0200

    Form practice task

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const form = document.getElementById('name-form');
 form.addEventListener('submit', consoleForm);
 
 function consoleForm(event) {
-  event.preventDefault()
+  event.preventDefault();
 
   const form = event.target;
   const value = form[0].value;
   console.log(value);
 }
 
+
+/*
+Створити форму в HTML
+В формі запитувати ім'я користувача
+
+При відправленні форми - привітайте користувача ("привіт ім'я користувача")
+Ім'я користувача візьміть з інпуту форми
+*/
+
+form.addEventListener('submit', consoleHello);
+
+function consoleHello (event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const value = form[0].value;
+  console.log(`Hello, ${value}!`);
+}
