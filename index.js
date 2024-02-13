1a887f4dfe402ce056daa485422df77c8b3a397e
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-13 17:51:33 +0200

    Another example

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const button = document.querySelector('button');
 button.addEventListener('click', btnClickHandler);
 
 function btnClickHandler(event) {
+  console.log('Hi from button click handler');
   // console.log(event.composedPath());
-  console.log(event.target); // той, на кому спрацювала подія
+  // console.log(event.target); // той, на кому спрацювала подія
   // target - елемент на якому сталася подія
   // target - елемент, до якого буде занурюватись подія
-  console.log(event.currentTarget); // той, якому належав обробник події
+  // console.log(event.currentTarget); // той, якому належав обробник події
   // currentTarget - елемент, якому належав обробник події
 }
+
+function bodyClickHandler(event) {
+  console.log('Hi from body click handler');
+}
+
+document.body.addEventListener('click', bodyClickHandler);
 
 const clickEvent = new MouseEvent('click');
 
