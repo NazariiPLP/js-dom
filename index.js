20e5c61ae05fe1d3545ef81b0b79eae2b46c0a4d
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-15 19:18:05 +0200

    Catch box game v2

--- index.js
+++ index.js
@@ -3,28 +3,21 @@
 Є div, який має переміститись на точку, в яку клацнули мишею
 */
 
-const field = document.querySelector("#game-field");
-const box = document.querySelector("#box");
+const field = document.querySelector('#game-field');
+const box = document.querySelector('#box');
 
-field.addEventListener("click", clickHandler, { capture: true });
+field.addEventListener('click', clickHandler, {capture: true});
 
 function clickHandler(event) {
-  // якщо ми нетискаємо НЕ на ігрове поле (тобто, на квадратик) - переміщення не відбувається
-  event.stopPropagation(); // це і є фікс помилки
+    // якщо ми натискаємо НЕ на ігрове поле (тобто, на квадратик) - переміщення не відбувається
+    event.stopPropagation(); // це і є фікс помилки
+    
+    // target - на якому елементі фактично відбулася подія (на який саме клікнули)
+    // currentTarget - якому елементу належав обробник події
+    if(event.currentTarget === event.target) { // якщо ми натискаємо на ігрове поле - переміщенню бути
+        const {target: {children: {box}}, clientX, clientY} = event;
 
-  // target - на якому елементі фактично відбулася подія (на який саме клікнули)
-  // currentTarget - якому елементу належав обробник події
-  if (event.currentTarget === event.target) { // якщо ми натискаємо на ігрове поле - переміщення відбувається; це і є фікс проблеми
-    const {
-      target: {
-        children: { box },
-      },
-      clientX,
-      clientY,
-    } = event; // undefined
-
-    box.style.top = `${clientY}px`; // undefined.style.top
-    box.style.left = `${clientX}px`; // undefined.style.left
-  }
+        box.style.top = `${clientY}px`;
+        box.style.left = `${clientX}px`;
+    }
 }
-
