05fcc6237863823312c36f65dd37c60f8834dd15
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-15 19:12:19 +0200

    Catch box game variant 1

--- index.js
+++ index.js
@@ -1,41 +1,30 @@
-function createDiv() {
-  const div = document.createElement("div"); // 1. Створення елементу у пам'яті
-  div.classList.add("box"); // 2. До цього елементу приєднумо стильовий клас
-  div.append("Hello world"); // 3. До цього елемену приєдуємо текстовий вузол
-  document.body.append(div); // 4. Приєднуємо текствоий елемент до body
-  /*
-  <div class="box">Hello world</div>
-  */
-  // btn.removeEventListener('click', createDiv);
-}
-
-const btnDiv = document.querySelector(".btn-div");
-btnDiv.addEventListener("click", createDiv);
-
 /*
-Спробуйте створити декілька HTML елементів через JS, (document.createElement)
-Спробуйте додати якісь стильові класи цим елементам. (element.classlist.add)
-Спробуйте додати якийсь текстовий вузол для елементів. ( element.append('text...')  )
-Причепіть створені елементи до body (  document.body.append(element)  )
+Є section, на якій треба клацати мишою
+Є div, який має переміститись на точку, в яку клацнули мишею
 */
 
-const btnHeader = document.querySelector('.btn-header');
-btnHeader.addEventListener('click', createHeader);
+const field = document.querySelector("#game-field");
+const box = document.querySelector("#box");
 
+field.addEventListener("click", clickHandler, { capture: true });
 
-function createHeader() {
-  const h1 = document.createElement('header');
-  h1.classList.add('header');
-  h1.append('Main header');
-  document.body.append(h1);
+function clickHandler(event) {
+  // якщо ми нетискаємо НЕ на ігрове поле (тобто, на квадратик) - переміщення не відбувається
+  event.stopPropagation(); // це і є фікс помилки
+
+  // target - на якому елементі фактично відбулася подія (на який саме клікнули)
+  // currentTarget - якому елементу належав обробник події
+  if (event.currentTarget === event.target) { // якщо ми натискаємо на ігрове поле - переміщення відбувається; це і є фікс проблеми
+    const {
+      target: {
+        children: { box },
+      },
+      clientX,
+      clientY,
+    } = event; // undefined
+
+    box.style.top = `${clientY}px`; // undefined.style.top
+    box.style.left = `${clientX}px`; // undefined.style.left
+  }
 }
 
-const btnParagraph = document.querySelector('.btn-paragraph');
-btnParagraph.addEventListener('click', createParapraph);
-
-function createParapraph() {
-  const paragraph = document.createElement('p');
-  paragraph.classList.add('center-paragrapg');
-  paragraph.append('Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim animi fugiat optio, quis repellendus sunt aperiam nostrum aut, autem, accusantium eum adipisci quod pariatur earum dolore cupiditate repellat illum nemo?');
-  document.body.append(paragraph);
-}
