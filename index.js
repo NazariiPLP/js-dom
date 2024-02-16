444c4da164f921839dc516ac81bd9c9438fa9c27
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-16 18:34:21 +0200

    Refactor

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 /*
+
 <article class="card-wrapper">
-<img class="card-image" src="https://masterpiecer-images.s3.yandex.net/487ca75268ea11eeaea6da477c0f1ee2:upscaled" alt="John avatar">
-<h2 class="username">John</h2>
-<p class="description">Description for John</p>
-</article> 
+        <img class="card-image" src="https://pm1.aminoapps.com/6750/dddd7fbf857879e7a4513d20a85c1c89d5d74a41v2_hq.jpg" alt="John avatar">
+        <h2 class="username">John</h2>
+        <p class="description">Description for John</p>
+</article>
+
 */
 
 const root = document.querySelector('#root');
 
 function createUserCard(user) {
-  // 1. Створення article
-  const article = document.createElement('article');
-  article.classList.add('card-wrapper')
+    // 1. Створення img
+    const img = document.createElement('img');
+    img.setAttribute('src', user.profilePicture);
+    img.setAttribute('alt', user.name);
+    img.classList.add('card-image');
 
-  // 2. Створення img
-  const img = document.createElement('img');
-  img.setAttribute('src', user.profilePicture);
-  img.setAttribute('alt', user.name);
-  img.classList.add('card-image');
+    // 2. Створення h2
+    const h2 = createElement('h2', {classNames: ['username']}, user.name);
 
-  // 3. Створення h2
-  const h2 = document.createElement('h2');
-  h2.append(user.name);
-  h2.classList.add('username');
+    // 3. Створення p
+    const p = createElement('p', {classNames: ['description']}, user.description);
 
-  // 4. Створення p
-  const p = document.createElement('p');
-  p.append(user.description);
-  p.classList.add('description');
-
-  // 5. Приєднати до article (п.1) елементи створенні в п. 2-4 (img, h2, p);
-  article.append(img, h2, p);
-
-  // 6. Повертаємо створений article
-  return article;
+    // 4. Створюємо і повертаємо article, в який вкладені створені img, h2, p
+    return createElement('article', {classNames: ['card-wrapper']}, img, h2, p);
 }
 
 const cardArray = data.map(user => createUserCard(user));
 
 root.append(...cardArray);
+
+/**
+ * @param {String} type - тег елемента, який нам треба створити
+ * @param {String[]} classNames - список класів, які треба додати до елемента
+ * @param {...Node} childNodes - список дочірніх вузлів
+ * @returns {HTMLElement}
+ */
+function createElement(type, {classNames}, ...childNodes) {
+    const elem = document.createElement(type);
+    elem.classList.add(...classNames);
+    elem.append(...childNodes);
+
+    return elem;
+}
