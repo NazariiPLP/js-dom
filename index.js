b58db6084b8dfaf4ec830141ba089e05bdccd8bc
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-20 20:26:00 +0200

    setInterval

--- index.js
+++ index.js
@@ -1,32 +1,25 @@
-/*
-Eventloop - механізм, який керує виконанням коду.
+// timeout - виконує функцію один раз після затримки
+// interval - виконуватиме функцію багато разів з затримкою
 
-Концептуально, ідея Event Loop полягає в тому, щоб не заблокувати основний потік даних.
-Технічно ця концепція реалізується через Google Chrome, який написаний на C++
-*/
-
-// setTimeout();
-
-// 1. завантажується сторінка
-// 2. реєструється таймаут
-// 3. після того, як пройде затримка - запускаємо якусь функцію
-
-function func() {
-    console.log(2 + 2);
-}
-
-// func();
-const timeoutID = setTimeout(func, 2000);
-
-func();
+// const intervalID = setInterval(() => {
+//     console.log('Logging')
+// }, 2000);
 
 /*
-Напишіть функцію, яка буде вітати юзера alert('Hello user!')
-Вітати функція має юзера не одразу, а тільки через 8 секунд, пясля того, як сторінка завантажиться
+Задача: кожну секунду буде виводити в консоль числа від 1 до 10
+Виконати цю задачу через інтервали 
 */
 
-function userGreetings() {
-    alert('Hello user!');
+function counter() {
+    let i = 1;
+    const intervalID = setInterval(() => {
+        console.log(i++);
+        if(i > 10) {
+            clearInterval(intervalID);
+            console.timeEnd();
+        }
+    }, 1000);
 }
 
-const helloTimeoutID = setTimeout(userGreetings, 8000);
+console.time('1');
+counter();
