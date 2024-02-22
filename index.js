a13eba427c2062f47ad9694cb3fbbd7da257b63b
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-22 20:44:46 +0200

    Creating promise

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-fetch('./uer.json')
-.then((response) => {
-  return response.json(); // перетворює нулі та одиниці на JS об'єкт
-})
-.then((data) => {
-  console.log(data);
-})
-.catch((err) => {
-  console.log(err);
-})
-.finally(() => {
-  console.log('FINALLY question closed');
+const p = new Promise(executor); // pending
+
+function executor (resolve, reject) {
+  // задача: знегерувати випадкове ціле число від 0 до 15
+  const number = Math.floor(Math.random() * 16);
+  
+  // якщо число кратне 2 -> резолвимо проміс та повертаємо це число
+  // якзл число не кратне 2 -> реджектимо проміс та повертаємо помилку
+
+  if (number % 2 === 0) {
+    resolve(number); //fulfilled
+  } else {
+    const err = new RangeError(`Error happanned: ${number}`);
+    reject(err); // reject
+  }
+}
+
+p.then((number) => {
+  console.log(number)
+}, (error) => {
+  console.log(error);
 });
