6222434626686cfca6be7e2c09e8e1fdf056549a
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-22 20:23:35 +0200

    Then, catch, finally

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-// fetch('./user.json')
-// .then((response) => {
-//   return response.json(); // перетворює нулі та одиниці на JS об'єкт
-// })
-// .then((data) => {
-//   console.log(data);
-// });
-
-fetch('https://api.monobank.ua/bank/currency')
+fetch('./uer.json')
 .then((response) => {
   return response.json(); // перетворює нулі та одиниці на JS об'єкт
 })
 .then((data) => {
   console.log(data);
-});
-
-
-fetch('https://api.privatbank.ua/p24api/pubinfo?exchange&coursid=5')
-.then((response) => {
-  return response.json(); // перетворює нулі та одиниці на JS об'єкт
 })
-.then((data) => {
-  console.log(data);
+.catch((err) => {
+  console.log(err);
+})
+.finally(() => {
+  console.log('FINALLY question closed');
 });
