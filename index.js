08f6b0266bee4ceabc65ae4e8941c716cbd4b8b3
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-24 15:36:48 +0200

    setTimeout in promise

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
-const p = new Promise(executor); // pending
+const p = new Promise();
 
-function executor (resolve, reject) {
-  // задача: знегерувати випадкове ціле число від 0 до 15
-  const number = Math.floor(Math.random() * 16);
-  
-  // якщо число кратне 2 -> резолвимо проміс та повертаємо це число
-  // якзл число не кратне 2 -> реджектимо проміс та повертаємо помилку
-
-  if (number % 2 === 0) {
-    resolve(number); //fulfilled
-  } else {
-    const err = new RangeError(`Error happanned: ${number}`);
-    reject(err); // reject
-  }
+function executor(resolve, reject) {
+  setTimeout(() => {
+    resolve('It time'); // fulfiled
+  }, 15000);
 }
 
-p.then((number) => {
-  console.log(number)
-}, (error) => {
-  console.log(error);
+const h1 = document.querySelector('#root');
+
+p.then((str) => {
+  h1.append(str);
 });
+
