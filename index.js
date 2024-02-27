7dc8e10455cd393f0219829aadd0ebe3bbbc2ae8
Nazarii Pylypenko <nazariipylypenko22@gmail.com>
2024-02-27 17:15:57 +0200

    User-card task

--- index.js
+++ index.js
@@ -1,60 +1,85 @@
 /*
-https://api.openweathermap.org/data/2.5/weather?q=Dnipro&appid=f7c576ba3699bdd0b98ddcf196639992&units=metric
+Задача 1
 
+У вас є сторінка.
+На сторінці є кнопка "підвантажити юзера".
+При натисненні на цю кнопку, на сторінку має додатися картка юзера.
 
+Картка може містити:
+- аватарка
+- ім'я, прізвище
+- імейл
+- країна
+- вік
 
-f7c576ba3699bdd0b98ddcf196639992
-
-
-// Задача: зробити погодний віджет
-
-Алгоритм вирішення:
-+ 1. Зробити верстку елементів, які отримують від користувача дані про місто
-+ 2. Отримати дані з апі і обробити їх (підготувати дані для відмалювання у верстці)
-3. Зробити картку з погодою і відобразити її
-
+https://randomuser.me/api
 */
 
-const API_KEY = 'f7c576ba3699bdd0b98ddcf196639992';
-const API_BASE = 'https://api.openweathermap.org/data/2.5/weather';
+const API_BASE = 'https://randomuser.me/api';
 
 const btn = document.querySelector('.btn');
 
 btn.addEventListener('click', buttonClickHandler);
 
 function buttonClickHandler({target}) {
-  const selectValue = target.previousElementSibling.value;
-  requestAPI(selectValue);
+  requaestApi(API_BASE);
 }
 
-function requestAPI(cityName) {
-  // 1. Готуємо URL
-  const url = `${API_BASE}?q=${cityName}&appid=${API_KEY}&units=metric`;
+function requaestApi(API_BASE){
+  const url = API_BASE;
 
-  // 2. Робимо запити
   fetch(url)
   .then((response) => {
     return response.json();
   })
   .then((data) => {
-    // 3. Відмальовуємо погоду
-    console.log(data);
-    displayWeather(data);
-  })
-}
+    console.log(data)
+    displayUser(data)
+  });
+};
 
-function displayWeather(weatherObject) {
-  const {name, main: {temp}, weather: [{description}] } = weatherObject;
+function displayUser(data) {
+  // Get avatar image
+  const {results: [{picture}]} = data;
+  const avatar = picture.large;
+
+  // Get name, lastname
+  const {results: [{name}]} = data;
+  const {first} = name;
+  const firstName = first;
+  const {last} = name;
+  const lastName = last;
+
+  // Get email
+  const {results: [{email}]} = data;
+  console.log(email);
+
+  // Get country
+  const {results: [{location}]} = data;
+  const country = location.country;
+  console.log(country);
+
+  //Get age
+  const {results: [{dob}]} = data;
+  const age= dob.age;
+  console.log(age);
   
-  const article = document.querySelector('#weather-box');
-  article.classList.add('weather-display');
+  const section = document.querySelector('#wrapper');
+  section.classList.add('wrapper');
 
-  const city = document.querySelector('#city');
-  city.textContent = name;
+  const userImg = document.querySelector('#user-img');
+  userImg.classList.add('user-img');
+  userImg.setAttribute('src', avatar);
 
-  const temperature = document.querySelector('#temp');
-  temperature.textContent = `${temp}°C`;
+  const h1 = document.querySelector('#name');
+  h1.textContent = `${firstName} ${lastName}`;
 
-  const desc = document.querySelector('#description');
-  desc.textContent = description;
+  const emailP = document.querySelector('#email');
+  emailP.textContent = `Email: ${email}`;
+
+  const counstryP = document.querySelector('#country');
+  counstryP.textContent = `Country: ${country}`;
+  
+  const ageP = document.querySelector('#age');
+  ageP.textContent = `Age: ${age}`;
 }
